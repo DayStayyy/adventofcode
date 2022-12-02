972f47d6cc17c8a9315af5122aca2605cbd4fb77
DayStayyy <daystayyy@gmail.com>
2022-12-02 22:43:42 +0100

    part 2

--- day2/day_2.go
+++ day2/day_2.go
@@ -30,11 +30,23 @@ func main() {
 		'C': {'X': 6, 'Y': 0, 'Z': 3},
 	}
 
+	choices_seconde_part := map[byte]map[byte]byte{
+		'A': {'X': 'Z', 'Y': 'X', 'Z': 'Y'},
+		'B': {'X': 'X', 'Y': 'Y', 'Z': 'Z'},
+		'C': {'X': 'Y', 'Y': 'Z', 'Z': 'X'},
+	}
+
 	points := 0
+	points_second_part := 0
+	choice := byte('A')
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		points += letter_points[scanner.Text()[2]]
 		points += rock_paper_scissors[scanner.Text()[0]][scanner.Text()[2]]
+		choice = choices_seconde_part[scanner.Text()[0]][scanner.Text()[2]]
+		points_second_part += letter_points[choice]
+		points_second_part += rock_paper_scissors[scanner.Text()[0]][choice]
 	}
 	fmt.Println("Part 1 : \nYour number of points is ", points)
+	fmt.Println("Part 2 : \nYour number of points is ", points_second_part)
 }
