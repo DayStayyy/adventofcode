cf82ed5146b7e4187cee7a71cac1696b32241a29
DayStayyy <daystayyy@gmail.com>
2022-12-04 13:19:45 +0100

    day 4

--- day3/day_3.go
+++ day3/day_3.go
@@ -21,12 +21,33 @@ func main() {
 	}()
 
 	priorities_sum := 0
+	priorities_sum_mart_two := 0
+	var group []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		priorities_sum += calcul_priorities_sum_of_rucksack(scanner.Text()[0:len(scanner.Text())/2], scanner.Text()[len(scanner.Text())/2:])
+		group = append(group, scanner.Text())
+		if len(group) == 3 {
+			priorities_sum_mart_two += common_letters(group)
+			group = []string{}
+		}
 	}
 
 	fmt.Println("Part 1 : \nThe sum of the priorities is", priorities_sum)
+	fmt.Println("Part 2 : \nThe sum of the priorities is", priorities_sum_mart_two)
+
+	// var list [6]string
+	// list[0] = "vJrwpWtwJgWrhcsFMMfFFhFp"
+	// list[1] = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+	// list[2] = "PmmdzqPrVvPwwTWBwg"
+	// list[3] = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+	// list[4] = "ttgJtRGJQctTZtZT"
+	// list[5] = "CrZsJsPPZsGzwwsLwLmpwMDw"
+
+	// for _, word := range list {
+	// 	priorities_sum += calcul_priorities_sum_of_rucksack(word[0:len(word)/2], word[len(word)/2:])
+	// }
+
 }
 
 func calcul_priorities_sum_of_rucksack(stringA, stringB string) int {
@@ -47,4 +68,13 @@ func to_number(letter rune) int {
 	} else {
 		return int(letter) - 38
 	}
+}
+
+func common_letters(group []string) int {
+	for _, letter := range group[0] {
+		if strings.Contains(group[1], string(letter)) && strings.Contains(group[2], string(letter)) {
+			return to_number(letter)
+		}
+	}
+	return 0
 }
---
+++ day4/day_4.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./day4/data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	nb_of_pairs := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		assignements := strings.FieldsFunc(scanner.Text(), Split)
+		a1, _ := strconv.ParseInt(assignements[0], 10, 64)
+		b1, _ := strconv.ParseInt(assignements[1], 10, 64)
+		a2, _ := strconv.ParseInt(assignements[2], 10, 64)
+		b2, _ := strconv.ParseInt(assignements[3], 10, 64)
+
+		if (a1 <= a2 && b1 >= b2) || (a2 <= a1 && b2 >= b1) {
+			fmt.Println(assignements[0], assignements[1], assignements[2], assignements[3])
+			nb_of_pairs++
+		}
+
+	}
+	fmt.Println("Part 1 : \nThe number of pairs is", nb_of_pairs)
+
+}
+
+func Split(r rune) bool {
+	return r == ',' || r == '-'
+}
